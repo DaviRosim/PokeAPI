5a528ba93c9f2a8f830f376d55ceb87ae2fd7873
DaviRosim <d.rosim@4intelligence.ai>
2025-09-22 12:47:27 -0300

    add: pokemons services

---
+++ app/services/__init__.py
@@ -0,0 +1,1 @@
+from services.pokemon import PokeService
---
+++ app/services/pokemon.py
@@ -0,0 +1,51 @@
+import json
+from fastapi import HTTPException
+from bson.json_util import dumps
+
+from repositories import PokeRepository
+from models import Pokemon
+
+
+class PokeService:
+
+    def __init__(self):
+        self.__repository: PokeRepository = PokeRepository()
+
+    def get_by_id(self, pokemon_id: int):
+        pokemon = self.__repository.get_by_id(pokemon_id)
+        pokemon = json.loads(dumps(pokemon))
+
+        if not pokemon:
+            raise HTTPException(status_code=404,
+                                detail="Pokemon not found")
+
+        return pokemon
+
+    def get(self, page, page_size):
+        pokemons = self.__repository.get(page, page_size)
+        pokemons = json.loads(dumps(pokemons))
+        return pokemons
+
+    def create(self, pokemon: Pokemon):
+        created_pokemon = str(self.__repository.create(pokemon))
+        if not created_pokemon:
+            raise HTTPException(status_code=400,
+                                detail="Error creating pokemon")
+
+        return created_pokemon
+
+    def update(self, pokemon_id, pokemon: Pokemon):
+        updated_pokemon = self.__repository.update(pokemon_id, pokemon)
+        if not updated_pokemon:
+            raise HTTPException(status_code=404,
+                                detail="Pokemon not found")
+
+        return updated_pokemon
+
+    def delete(self, pokemon_id):
+        deleted_pokemon = self.__repository.delete(pokemon_id)
+        if not deleted_pokemon:
+            raise HTTPException(status_code=404,
+                                detail="Pokemon not found")
+
+        return deleted_pokemon
